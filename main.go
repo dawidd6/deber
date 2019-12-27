f2581037ec5ead364853b462a645bb9847b05862
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-27 13:40:37 +0100

    main: release v1.0.0

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ const (
 	// Program is the name of program
 	Program = "deber"
 	// Version of program
-	Version = "0.6.2"
+	Version = "1.0.0"
 	// Description of program
 	Description = "Debian packaging with Docker."
 )
