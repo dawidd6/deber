608ff37eca8b4a41a40bbbf84b42d82fb53f49ea
Denis Proskurin <dpvpro@gmail.com>
2021-08-30 12:52:18 +0300

    additional dependency for image

--- pkg/dockerfile/dockerfile.go
+++ pkg/dockerfile/dockerfile.go
@@ -37,7 +37,7 @@ RUN printf "Package: *\nPin: origin \"\"\nPin-Priority: 990\n" > /etc/apt/prefer
 # Install required packages.
 RUN apt-get update && \
 	apt-get install --no-install-recommends -y \
-	build-essential devscripts debhelper lintian fakeroot dpkg-dev
+	build-essential devscripts debhelper lintian fakeroot dpkg-dev libperl-dev libssl-dev
 
 # Set working directory.
 WORKDIR {{ .SourceDir }}
